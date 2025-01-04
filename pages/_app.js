f0f7e21852c26beb1244b15207ba6937a9f8a33b
StefanieRu <162427892+StefanieRu@users.noreply.github.com>
2025-01-04 15:45:43 +0100

    240104_Aktualisierung

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import Checkbox from "@mui/material/Checkbox";
+
+export default function App() {
+  return (
+    <>
+      <Checkbox />
+      <h1>Hallo2 App</h1>
+    </>
+  );
+}
---
+++ pages/info.js
@@ -0,0 +1,7 @@
+export default function Info() {
+  return (
+    <>
+      <h1>Hallo2 Info</h1>
+    </>
+  );
+}
