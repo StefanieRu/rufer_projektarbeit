1d8336777a5ceb4d564dd0398bfeda573939d3de
StefanieRu <162427892+StefanieRu@users.noreply.github.com>
2025-01-17 19:27:51 +0100

    240117_Abgabe04

--- pages/_app.js
+++ pages/_app.js
@@ -24,6 +24,7 @@ export default function App() {
       .get("https://ruferprojektarbeit.vercel.app/api/daten")
       .then((response) => {
         const JSONDaten = response.data;
+        console.log(JSONDaten);
 
         const JSONStandorte = [
           ...new Set(JSONDaten.map((item) => item.Standortname)),
